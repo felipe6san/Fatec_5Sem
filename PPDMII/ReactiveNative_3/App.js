128dca28f599b725a75ce211fcda802e7a608a8a
Felipe <felipe6san@hotmail.com>
2025-08-20 20:33:55 -0300

    Migrate projeto2 to ProjectBlank and ReactiveNative_3
    Renamed and reorganized the 'projeto2' directory into two new projects: 'ProjectBlank' and 'ReactiveNative_3'. Added new entry files and configuration for both projects, moved assets, and removed unused files from the original 'projeto2'.

---
+++ PPDMII/ReactiveNative_3/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+ 
+export default function App() {
+  const [cep, setCep] = useState("");
+ 
+  const buscaCep = ( value ) => {
+    let url = `https://viacep.com.br/ws/${value}/json/`;
+    fetch(url)
+      .then((response) => {return response.json()})
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar CEP:", error);
+      });
+  }
+ 
+  return (
+    <View style={styles.container}>
+      <Text>Digite o CEP:</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          width: 200,
+          marginTop: 10,
+        }}
+        placeholder="123456789"
+        keyboardType="numeric"
+        onChangeText={(text) => {
+          setCep(text);
+        }}
+      />
+      <Pressable>
+        style={{
+          backgroundColor: "#2196F3",
+          padding: 10,
+          marginTop: 10,
+        }}
+        onPress={() => {
+          buscaCep(cep);
+        }}
+      </Pressable>
+    </View>
+  );
+}
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
